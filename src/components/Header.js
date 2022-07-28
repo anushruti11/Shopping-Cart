6df73e01e73cb032dbf5e42ab30e3770d276731b
anushruti11 <anushruti1132001@gmail.com>
2022-07-28 21:36:09 +0530

    Header added

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, FormControl, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="/">Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            type="search"
+            placeholder="Search a product..."
+            className="m-auto"
+            aria-label="Search"
+          />
+        </Navbar.Text>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
